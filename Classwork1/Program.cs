99d893b2ac74b394ab7de959d881e7582ba759f2
Acer <Acer@LAPTOP-L7FGK795>
2021-09-18 12:01:00 +0300

    Добавьте файлы проекта.

---
+++ Classwork1/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classwork1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("задание 1.1");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(Math.Round(a, 2));
+
+
+            Console.WriteLine("задание 1.2");
+            Console.WriteLine(Math.Round(Math.E, 1));
+
+
+            Console.WriteLine("задание 1.3");
+            string а = Console.ReadLine();
+            Console.WriteLine("Вы ввели число " + а);
+
+
+            Console.WriteLine("задание 1.4");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(a + "вот какое число Вы ввели");
+
+
+            Console.WriteLine("задание 1.5");
+            Console.WriteLine("1 13 49");
+
+
+            Console.WriteLine("задание 1.6");
+            Console.WriteLine("7.15  100");
+
+
+            Console.WriteLine("задание 1.7");
+            Console.WriteLine("Введите первое число: ");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите второе число: ");
+            double y = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите третье число: ");
+            double z = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(x + "  " + y + "  " + z);
+
+
+            Console.WriteLine("задание 1.8");
+            Console.WriteLine("Введите первое число: ");
+            double d = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите второе число: ");
+            double e = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите третье число: ");
+            double f = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите четвертое число: ");
+            double g = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(d + " " + e + " " + f + " " + g);
+
+
+            Console.WriteLine("задание 1.9");
+            Console.WriteLine(50);
+            Console.WriteLine(10);
+
+
+            Console.WriteLine("задание 1.10");
+            Console.WriteLine(5);
+            Console.WriteLine(10);
+            Console.WriteLine(21);
+
+
+            Console.WriteLine("задание 1.11");
+            Console.WriteLine("Введите первое число: ");
+            double k = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите второе число: ");
+            double l = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите третье число: ");
+            double m = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите четвертое число: ");
+            double n = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(k);
+            Console.WriteLine(l);
+            Console.WriteLine(m);
+            Console.WriteLine(n);
+
+
+            Console.WriteLine("задание 1.12");
+            Console.WriteLine("5 10");
+            Console.WriteLine("7 см");
+
+
+            Console.WriteLine("задание 1.13");
+            Console.WriteLine("2 кг");
+            Console.WriteLine("13 17");
+
+
+
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Homework/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Упражнение 2.1");
+            Console.WriteLine("Как Вас зовут?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Здравствуйте, " + name);
+
+
+            Console.WriteLine("Упражнение 2.2");
+            Console.WriteLine("Введите два целых числа");
+            double x, y;
+            x = Convert.ToDouble(Console.ReadLine());
+            y = Convert.ToDouble(Console.ReadLine());
+            if (y != 0)
+            {
+                Console.WriteLine(x/y); }
+            else
+            { Console.WriteLine("Ошибка, на ноль делить нельзя"); }
+
+
+            Console.WriteLine("Домашняя работа 2.1");
+            char bukovka = Console.ReadKey().KeyChar;
+            char letter = char.ToLower(bukovka);
+            char nextBukovka;
+            if (letter == 'z')
+            {
+                nextBukovka = 'a';
+            }
+            else
+            {
+                nextBukovka = (char)(((int)letter) + 1);
+            }
+            Console.WriteLine();
+            Console.WriteLine("{0}", nextBukovka);
+
+
+            Console.WriteLine("Домашняя работа 2.2");
+            Console.WriteLine("Введите коэффициенты квадратного уравнения");
+            double а = Convert.ToDouble(Console.ReadLine());
+            double б = Convert.ToDouble(Console.ReadLine());
+            double с = Convert.ToDouble(Console.ReadLine());
+            double d = б * б - 4 * а * с;
+            if (d >= 0)
+            {
+                double k = Math.Sqrt(d);
+                double x1 = (-б + k) / (2 * а);
+                double x2 = (-б - k) / (2 * а);
+                Console.WriteLine(x1 + " " + x2);
+            }
+            else
+            { Console.WriteLine("У уравнения нет корней"); }
+            
+
+            
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
